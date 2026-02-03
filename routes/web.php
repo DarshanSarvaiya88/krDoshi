994f7efc6be9491c0fd812b6a517387ee7b3e5be
itsRD72 <rickroy121272@gmail.com>
2026-02-03 17:44:34 +0530

    moved a file to proper location
    added missing js and css files

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::view('add-center-form','add-center-form');
-Route::view('add-course-form','add-course-form');
-Route::view('add-batch-form','add-batches-form');
-
-Route::view('add-mcq-form','add-mcq-form');
-Route::view('test','test');
-
-Route::get('add-student-form', [StudentController::class, 'create']); 
-Route::post('/students', [StudentController::class, 'store']);
+Route::get('/',[DashboardController::class,'dashboard']);
 
